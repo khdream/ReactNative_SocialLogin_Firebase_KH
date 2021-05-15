81340ecfb97b9e6a4d3e83ce9df53d90780169ff
khdream <khdream94@yandex.com>
2021-05-14 23:29:17 -0700

    514

--- App.js
+++ App.js
@@ -2,39 +2,43 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Button, TouchableOpacity, Text} from 'react-native';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
-import { LoginManager, AccessToken } from 'react-native-fbsdk';
-import LinkedInModal from 'react-native-linkedin'
+import {LoginManager, AccessToken} from 'react-native-fbsdk';
+// import LinkedInModal from 'react-native-linkedin';
 
-// FacebookSdk.sdkInitialize(getApplicationContext());
 
 GoogleSignin.configure({
   webClientId:
     '941917286127-sbaabdmlp8hnjf8ukd6htooui68triov.apps.googleusercontent.com',
 });
 
-
 export default class Main extends React.Component {
-
   onGooglePress = async () => {
-    await this.onGoogleButtonPress().then(() => console.log('Signed in with Google!'));
+    await this.onGoogleButtonPress().then(() =>
+      console.log('Signed in with Google!'),
+    );
   };
 
   onGoogleButtonPress = async () => {
     // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn();
+    const {idToken} = await GoogleSignin.signIn();
     // Create a Google credential with the token
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
     // Sign-in the user with the credential
     return auth().signInWithCredential(googleCredential);
-  }
+  };
 
   onFaceBookPress = async () => {
-    await this.onFacebookButtonPress().then(() => console.log('Signed in with Facebook!'));
+    await this.onFacebookButtonPress().then(() =>
+      console.log('Signed in with Facebook!'),
+    );
   };
-  
+
   onFacebookButtonPress = async () => {
     // Attempt login with permissions
-    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+    const result = await LoginManager.logInWithPermissions([
+      'public_profile',
+      'email',
+    ]);
     if (result.isCancelled) {
       throw 'User cancelled the login process';
     }
@@ -44,35 +48,45 @@ export default class Main extends React.Component {
       throw 'Something went wrong obtaining access token';
     }
     // Create a Firebase credential with the AccessToken
-    const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+    const facebookCredential = auth.FacebookAuthProvider.credential(
+      data.accessToken,
+    );
     // Sign-in the user with the credential
     return auth().signInWithCredential(facebookCredential);
-  }
-
-  linkedRef = React.createRef()
+  };
 
   logoutWithFacebook = () => {
     LoginManager.logOut();
     this.setState({userInfo: {}});
   };
 
-  render() {
+  linkedRef = React.createRef();
 
+  render() {
     return (
-      <View style={{ flex: 1, alignItems:'center', justifyContent:'center'}}>
-        <Button onPress={this.onGooglePress} title='google signin' />
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        {/* google */}
+        <Button onPress={this.onGooglePress} title="google signin" />
         <Text>{''}</Text>
-        <Button onPress={this.onFaceBookPress} title='facebook signin 1' />
+        {/* facebook */}
+        <Button onPress={this.onFaceBookPress} title="facebook signin 1" />
         <Text>{''}</Text>
-        <Button onPress={this.loginWithFacebook} title='facebook signin 2' />
+        <Button onPress={this.loginWithFacebook} title="facebook signin 2" />
         <Text>{''}</Text>
-        <LinkedInModal
+        {/* linkedin */}
+        {/* <LinkedInModal
           ref={this.linkedRef}
+          shouldGetAccessToken={false}
           clientID="78pm2g3m1yfza6"
           clientSecret="Edgt0T4tr3FlUSdT"
-          redirectUri="https://gofile.io"
-          onSuccess={token => console.log('####test',token)}
+          redirectUri="https://api.linkedin.com/v2/me"
+          linkText="Continue with Linkedin"
+          onSuccess={token => console.log('####test', token)}
         />
+        <Button
+          onPress={() => this.linkedRef.current.open()}
+          title="linkedin signin "
+        /> */}
 
         {/* <LinkedInModal
                 ref={this.linkedRef}
@@ -83,9 +97,6 @@ export default class Main extends React.Component {
                 linkText="Continue with Linkedin"
                 renderButton={this.renderButton}
             /> */}
-
-{/* onPress={() => this.linkedRef.current.open()}> */}
-
         {/* <Button title="Log Out" onPress={this.linkedRef.current.logoutAsync()} /> */}
       </View>
     );
